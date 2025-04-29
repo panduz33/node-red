3e647cf93647c9dcd81ba2b4717c0c4ab805bc36
pandu.setiawan <pandu.setiawan@bytedance.com>
2025-04-29 09:07:11 +0700

    Init

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const RED = require('node-red');
+
+const app = express();
+const server = http.createServer(app);
+
+const settings = {
+    httpAdminRoot: "/red",
+    httpNodeRoot: "/api",
+    userDir: "./.nodered/",
+    functionGlobalContext: {} // This makes function nodes work
+};
+
+RED.init(server, settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(process.env.PORT || 1880, () => {
+    console.log(`Server running on port ${process.env.PORT || 1880}`);
+});
+
+RED.start();
